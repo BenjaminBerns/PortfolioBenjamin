e6c0c3776f04827fb6a95814f3b96debc4df9582
Benji <92870359+BenjaminBerns@users.noreply.github.com>
2024-02-26 22:46:16 +0100

    Add files via upload

---
+++ Benjamin_BERNS_Portfolio/portfolio-main/Public/stage.js
@@ -0,0 +1,4 @@
+const tl_stage = gsap.timeline();
+
+tl_stage
+    .from('.title', {y: 100, duration: 1, opacity: 0}, 0.5)
---
+++ Benjamin_BERNS_Portfolio/portfolio-main/Traitement.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Votre Nom est :';
+echo $_POST["nom"];
+echo "<br>";
+
+echo 'votre mail est';
+echo $_POST["mail"];
+echo "<br>";
+
+echo "Vous avez demandé d'envoyer un courrier de notification";
+echo $_POST["check"];
+echo "<br>";
+
+echo "Votre type d'abonnement :";
+echo $_POST["r"];
+echo "<br/>";
